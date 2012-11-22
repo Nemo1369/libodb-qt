02a9b2b63483915e39aae0bec9bab541910305f9
Boris Kolpackov <boris@codesynthesis.com>
2012-11-22 09:01:03 +0200

    Bump version to 2.1.1

--- odb/qt/version.hxx
+++ odb/qt/version.hxx
@@ -43,8 +43,8 @@
 // since if the Qt interface virsion is incremented then the bugfix version
 // must be incremented as well.
 //
-#define LIBODB_QT_VERSION     2010000
-#define LIBODB_QT_VERSION_STR "2.1.0"
+#define LIBODB_QT_VERSION     2010100
+#define LIBODB_QT_VERSION_STR "2.1.1"
 
 #include <odb/post.hxx>
 
